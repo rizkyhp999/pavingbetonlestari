faeb1cce911f1128a207409a1fb11fb2cc3bb798
rizkyhp999 <rizkyhutamaputra@gmail.com>
2024-01-26 01:09:57 +0700

    keyword

--- components/hasilpekerjaan.tsx
+++ components/hasilpekerjaan.tsx
@@ -35,42 +35,6 @@ export default function page() {
   ];
   return (
     <section className="relative">
-      {/* Section background (needs .relative class on parent and next sibling elements) */}
-      <div className="relative  mx-auto px-4 sm:px-6">
-        <div className="pt-12 md:pt-20">
-          {/* Section header */}
-          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-            <h1 className="h2 mb-4">Hasil Pekerjaan Kami</h1>
-            <p className="text-2xl text-bold text-gray-600">
-              Hubungi kami :{" "}
-              <a
-                href="https://wa.me/620811723830"
-                className="text-green-800 hover:text-blue-600 "
-              >
-                0811-723-830
-              </a>
-            </p>
-          </div>
-          {/* Section content */}
-        </div>
-        <div className="flex flex-wrap justify-center items-center">
-          {images.map((imageSrc, index) => (
-            <a key={index} href={imageSrc}>
-              <Card className="max-w-sm m-5">
-                <Image
-                  width={500}
-                  height={500}
-                  src={imageSrc}
-                  alt={`image ${index + 1}`}
-                />
-                <h5 className="text-2xl font-bold text-center tracking-tight text-gray-900 ">
-                  {captions[index]}
-                </h5>
-              </Card>
-            </a>
-          ))}
-        </div>
-      </div>
       <div className="text-[1px] text-white">
         <p>
           Pagar Beton Berkualitas Panel Beton Tahan Lama Paving Beton Terbaik
@@ -156,6 +120,42 @@ export default function page() {
           Profesional Panel Beton Lampung Profesional Paving Beton Lampung
           Profesional Fiberglass Lampung Profesional
         </p>
+      </div>
+      {/* Section background (needs .relative class on parent and next sibling elements) */}
+      <div className="relative  mx-auto px-4 sm:px-6">
+        <div className="pt-12 md:pt-20">
+          {/* Section header */}
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
+            <h1 className="h2 mb-4">Hasil Pekerjaan Kami</h1>
+            <p className="text-2xl text-bold text-gray-600">
+              Hubungi kami :{" "}
+              <a
+                href="https://wa.me/620811723830"
+                className="text-green-800 hover:text-blue-600 "
+              >
+                0811-723-830
+              </a>
+            </p>
+          </div>
+          {/* Section content */}
+        </div>
+        <div className="flex flex-wrap justify-center items-center">
+          {images.map((imageSrc, index) => (
+            <a key={index} href={imageSrc}>
+              <Card className="max-w-sm m-5">
+                <Image
+                  width={500}
+                  height={500}
+                  src={imageSrc}
+                  alt={`image ${index + 1}`}
+                />
+                <h5 className="text-2xl font-bold text-center tracking-tight text-gray-900 ">
+                  {captions[index]}
+                </h5>
+              </Card>
+            </a>
+          ))}
+        </div>
       </div>
     </section>
   );
